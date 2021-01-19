652d2b93a6a66203a30418898d45288386c2246b
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-18 20:14:13 -0800

    Wire up google credentials and set callbackURL

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
 const app = express();
 
-// TODO: hook up Google OAuth credentials
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
