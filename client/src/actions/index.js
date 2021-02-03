bff2239b000849277a80ff5884a5d3c2c6c93b26
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-02 19:35:18 -0800

    Refactor .then promise to using async await syntax

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return function (dispatch) {
-    axios
-      .get('/api/current_user')
-      .then((resp) => dispatch({ type: FETCH_USER, payload: resp }));
-  };
+export const fetchUser = () => async (dispatch) => {
+  const resp = await axios.get('/api/current_user')
+  dispatch({ type: FETCH_USER, payload: resp });
 };
