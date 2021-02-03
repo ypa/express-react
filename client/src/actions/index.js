55d4205e65a542ae31763165e495ef82b47d0de0
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-02 19:41:47 -0800

    Fix action payload to return only resp's data

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => async (dispatch) => {
-  const resp = await axios.get('/api/current_user')
-  dispatch({ type: FETCH_USER, payload: resp });
+  const resp = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: resp.data });
 };
