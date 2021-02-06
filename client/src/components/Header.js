8053f567b7bd254b94dfdfb83a02aeb7a23b6ca7
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-05 20:55:13 -0800

    Add Stripe Payments component and render the checkout button on the Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -15,7 +15,14 @@ class Header extends Component {
           </li>
         );
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return [
+          <li>
+            <Payments />
+          </li>,
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>,
+        ];
     }
   }
 
@@ -26,7 +33,7 @@ class Header extends Component {
           <Link
             to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
-           >
+          >
             Feedback
           </Link>
           <ul className="right">{this.renderContent()}</ul>
