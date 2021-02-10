9b0577af9c8b02f771fd7be13f37b1f2afba958a
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-09 20:59:04 -0800

    Make sure express handle routes correctly in prod environment

--- index.js
+++ index.js
@@ -25,5 +25,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Ensure that Express will serve up production assets
+  app.use(expresss.static('client/build'));
+
+  // Ensure that Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
