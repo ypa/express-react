72b6387bbe0617caeb1f42bfdf1a1fd1707a3a0f
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-17 21:10:15 -0800

    Add Dashboard component with materialize icons

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+      <div class='fixed-action-btn'>
+        <a class='btn-floating btn-large red'>
+          <i class='large material-icons'>add</i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
