714248ee1e51ecfd892b3c8734e2e65b673a857f
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-18 20:29:38 -0800

    Create a place holder SurveyNew Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   componentDidMount() {
@@ -15,13 +15,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <BrowserRouter>
           <div>
             <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dashboard} />
+            <Route path='/surveys/new' component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
