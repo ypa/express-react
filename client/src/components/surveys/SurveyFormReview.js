a5c3ec74eefa1c38f3858afeb508bddce81d9741
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-24 19:25:48 -0800

    Add SurveyFormReview component and wire up in SurvyNew

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,15 @@
+// surveyFormReview shows user their form inputs for review
+import React from 'react';
+
+const SurveyFormReview = ({ onCancel }) => {
+  return (
+    <div>
+      <h5>Please confirm your entires</h5>
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+        Back
+      </button>
+    </div>
+  );
+};
+
+export default SurveyFormReview;
