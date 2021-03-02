599b9b1a581e498a3c36fa34e1511b55a5c6e04c
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-01 19:34:10 -0800

    Refactor processing of webhook events using lodash's chain

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -16,21 +16,20 @@ module.exports = (app) => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      const match = p.test(pathname);
-      if (match) {
-        return { email, surveyId: match.surveyId, choice: match.choice };
-      }
-    });
+    const p = new Path('/api/surveys/:surveyId/:choice');
 
-    // remove undefined
-    const compactEvents = _.compact(events);
-    // Remove duplicate same email + surveyId combo
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact() // remove undefined
+      .uniqBy('email', 'surveyId') // Remove duplicate same email + surveyId combo
+      .value();
 
-    console.log(uniqueEvents);
+    console.log(events);
     res.send({});
   });
 
