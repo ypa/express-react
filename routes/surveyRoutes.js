4d7e4acc577693da155eb7c60ecba6d368a5088e
Yan P Aung <yan.pye.aung@gmail.com>
2021-03-02 19:51:43 -0800

    Fix the survey email thank you page route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
 
